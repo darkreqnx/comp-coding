72820e7333b3f05f4cb747636645f413262cf7a7
darkreqnx <niks.nair@gmail.com>
2019-02-18 23:08:16 +0530

    initial

---
+++ inplace-mergesort.cpp
@@ -0,0 +1,96 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void inplace_merge(int *arr, int start, int mid, int end){
+	//start, start2 pointers to both arrays
+	int start2 = mid + 1;
+
+	//already sorted
+	if(arr[mid] <= arr[start2])
+		return;
+
+	while(start <= mid && start2 <= end){
+		if(arr[start] <= arr[start2])
+			start++;
+		else{
+			int value = arr[start2];
+			int index = start2;
+
+			while(index != start){
+				arr[index] = arr[index - 1];
+				index--;
+			}
+			arr[start] = value;
+
+			start++;
+			mid++;
+			start2++;
+		}
+	}
+}
+
+void vanilla_merge(int *arr, int l, int m, int r){
+	int n1 = m - l + 1; int n2 = r - m;
+	
+	int left[n1], right[n2];
+
+	for(int i = 0; i < n1; i++)
+		left[i] = arr[l + i];
+	for(int j = 0; j < n2; j++)
+		right[j] = arr[m + 1 + j];
+
+	int i = 0; int j = 0; int k = l;
+
+	while(i < n1 && j < n2){
+		if(left[i] <= right[j]){
+			arr[k] = left[i];
+			i++;
+		}
+		else{
+			arr[k] = right[j];
+			j++;
+		}
+		k++;
+	}
+
+	while(i < n1){
+		arr[k] = left[i];
+		i++; k++;
+	}
+	while(j < n2){
+		arr[k] = right[j];
+		j++; k++;
+	}
+}
+
+//same as vanilla mergesort
+void mergeSort(int *arr, int l, int r){
+	if(l < r){
+		int m = l + (r - l) / 2;
+
+		mergeSort(arr, l, m);
+		mergeSort(arr, m + 1, r);
+
+		// inplace_merge(arr, l, m, r);
+		vanilla_merge(arr, l, m, r);
+	}
+}
+
+void printArray(int *arr, int n){
+	for(int i = 0; i < n; i++)
+		cout << arr[i] << " "; 
+	cout << endl;
+}
+
+int main(){
+	int n;
+	cin >> n;
+	int arr[n];
+	for(int i = 0; i < n; i++)
+		cin >> arr[i];
+
+	mergeSort(arr, 0, n - 1);
+	printArray(arr, n);
+
+    return 0;
+}
