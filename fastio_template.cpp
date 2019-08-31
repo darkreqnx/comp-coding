64772ecf431e1df71352bf180c4c9a2a2931304b
Nikhil S Nair <35428677+darkreqnx@users.noreply.github.com>
2019-08-31 23:55:47 +0530

    Create fastio_template.cpp

---
+++ fastio_template.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    // "\n" instead of endl -- since endl flush won't be required for new line for large outputs
+    
+    return 0;
+}
